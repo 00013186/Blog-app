17469d7d59d3fa17e3c157237f9cedbe8e5fd87d
Azzberg <azik0@icloud.com>
2023-04-03 05:00:42 +0500

    added update and delete codes

--- app.js
+++ app.js
@@ -64,6 +64,45 @@ app.post('/add', (req, res) =>{
     }
 })
 
+app.get('/:id/delete', (req, res) =>{
+    const id = req.params.id
+
+    fs.readFile('./data/blogs.json', (err, data) =>{
+        if (err) throw err
+        
+        const todos = JSON.parse(data)
+
+
+        const filteredTodos = todos.filter(todo => todo.id !== id)
+
+
+        fs.writeFile('./data/blogs.json', JSON.stringify(filteredTodos), (err) => {
+            if (err) throw err
+
+            res.render('home', {todos: filteredTodos, delete: true})
+        })
+    })
+})
+
+app.get('/:id/update', (req, res)=>{
+    const id = req.params.id
+
+    fs.readFile('./data/blogs.json', (err, data)=>{
+    if (err) throw err
+    const todos = JSON.parse(data)
+    const todo = todos.filter(todo => todo.id == req.params.id)[0]
+    const todoIdx = todos.indexOf(todo)
+    const splicedTodo = todos.splice(todoIdx,1)[0]
+    splicedTodo.done = true
+    todos.push[splicedTodo]
+    fs.writeFile('./data/blogs.json',JSON.stringify(todos),(err)=>{
+        if (err) throw err
+        res.render('home', {todos: todos})
+    })
+    })
+    
+})
+
 app.listen(PORT, (err) => {
     if (err) throw err
 
